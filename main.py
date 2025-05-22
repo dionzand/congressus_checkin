100424de4f878328d92208808a7475fbe31c7e89
dionzand <105492790+dionzand@users.noreply.github.com>
2025-05-22 13:57:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import streamlit_authenticator as stauth
 
 # Constants
 API_KEY = os.getenv("API_KEY")
-EVENT_ID = "113757"
+EVENT_ID = "113758"
 BASE_URL = "https://api.congressus.nl/v30"
 HEADERS = {"Authorization": f"Bearer {API_KEY}", "Content-Type": "application/json"}
 
@@ -69,7 +69,7 @@ def get_member_status(member_id):
     return "".join(member_statuses)
 
 
-st.title("Domibo 24-04-2025")
+st.title("Domibo 22-05-2025")
 
 # Render the login widget
 try:
